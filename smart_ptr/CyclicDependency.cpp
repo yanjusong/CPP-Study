79803483e7a5da5df5cf61d2f2f23b617f96fe94
yanjusong <1023651409@qq.com>
2019-02-13 21:26:14 +0800

    shard_ptr循环引用示例

---
+++ smart_ptr/CyclicDependency.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+using namespace std;
+
+class ClassB;
+
+class ClassA
+{
+  public:
+    ClassA() { cout << "ClassA Constructor..." << endl; }
+    ~ClassA() { cout << "ClassA Destructor..." << endl; }
+    shared_ptr<ClassB> pb; // 在A中引用B
+};
+
+class ClassB
+{
+  public:
+    ClassB() { cout << "ClassB Constructor..." << endl; }
+    ~ClassB() { cout << "ClassB Destructor..." << endl; }
+    shared_ptr<ClassA> pa; // 在B中引用A
+};
+
+int main()
+{
+    // in this situation will cause Cyclic Dependency.
+    shared_ptr<ClassA> spa(new ClassA);
+    shared_ptr<ClassB> spb = make_shared<ClassB>();
+    spa->pb = spb;
+    spb->pa = spa;
+
+    return 0;
+}
