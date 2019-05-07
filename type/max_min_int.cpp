b0a515008c889857a2590ff1007289d01699205a
yanjusong <1023651409@qq.com>
2019-05-07 21:17:43 +0800

    int的最大最小值

--- type/max_min_int.cpp
+++ type/max_min_int.cpp
@@ -4,24 +4,28 @@ using namespace std;
 
 int main()
 {
-    cout << "maximum int:" << INT_MAX << endl;
-    cout << "minimum int:" << INT_MIN << endl;
+    cout << "INT_MAX:" << INT_MAX << endl;
+    cout << "INT_MIN:" << INT_MIN << endl;
 
     int maxNum = 0x7fffffff;
-    int minNum = -maxNum - 1;
+    int minNum1 = -maxNum - 1;
+    int minNim2 = 0x80000000;
     cout << "maxNum=" << maxNum << endl;
-    cout << "minNum=" << minNum << endl;
+    cout << "minNum1=" << minNum1 << endl;
+    cout << "minNim2=" << minNim2 << endl;
 
     unsigned int uMaxNum = 0xffffffff;
     unsigned int uMinNum = 0;
     cout << "uMaxNum=" << uMaxNum << endl;
     cout << "uMinNum=" << uMinNum << endl;
 
-    // 错误：无限循环了
-    for (unsigned char i = 10; i >= 0; --i) 
-    {
-        cout << (int)i << " ";
-    }
+    // unsigned int v = uMinNum - 1;
+    // cout << "v=" << v << endl;
+    // 错误：无限循环
+    // for (unsigned char i = 10; i >= 0; --i) 
+    // {
+    //     cout << (int)i << " ";
+    // }
 
     return 0;
 }
